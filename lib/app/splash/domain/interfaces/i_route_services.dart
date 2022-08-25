2a1babfda54ae95b28c75d9719514280fbf96990
Bernava <bernava@hugtak.com>
2022-08-24 23:13:03 -0300

    Refactor IRouteServices

--- lib/app/splash/domain/interfaces/i_route_services.dart
+++ lib/app/splash/domain/interfaces/i_route_services.dart
@@ -1,7 +1,7 @@
 import 'package:dartz/dartz.dart';
-import 'package:nu_bank_clone/app/splash/domain/entities/splash_props.dart';
+import 'package:nu_bank_clone/app/splash/domain/entities/loading_params.dart';
 import 'package:nu_bank_clone/app/splash/error/route_exception.dart';
 
 abstract class IRouteServices {
-  Future<Either<RouteException, bool>> redirectTo(SplashParams params);
+  Future<Either<RouteException, bool>> redirectTo(LoadingParams params);
 }
